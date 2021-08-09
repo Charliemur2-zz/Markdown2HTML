9805cdd52cb7bea9806c79224a083a29211a7c60
Charliemur2 <charliemurciamunoz@gmail.com>
2021-08-08 23:17:58 -0500

    create markdown2html.py

--- markdown2html.py
+++ markdown2html.py
@@ -15,24 +15,24 @@ def markdownToHtml():
               file=sys.stderr)
         exit(1)
 
-    md_file = sys.argv[1]
-    html_file = sys.argv[2]
+    mark_down = sys.argv[1]
+    html = sys.argv[2]
 
-    if not(os.path.isfile("./{}".format(md_file))):
-        print("Missing {}".format(md_file), file=sys.stderr)
+    if not(os.path.isfile("./{}".format(mark_down))):
+        print("Missing {}".format(mark_down), file=sys.stderr)
         exit(1)
 
-    headings_md = ["###### ", "##### ", "#### ", "### ", "## ", "# "]
-    headings_html = ["<h6>", "<h5>", "<h4>", "<h3>", "<h2>", "<h1>"]
+    head_mark = ["###### ", "##### ", "#### ", "### ", "## ", "# "]
+    head_html = ["<h6>", "<h5>", "<h4>", "<h3>", "<h2>", "<h1>"]
 
-    with open(md_file, "r") as f_in:
-        with open(html_file, "w") as f_out:
+    with open(mark_down, "r") as f_in:
+        with open(html , "w") as f_out:
             for line in f_in.read().split("\n"):
-                for i in range(0, len(headings_md)):
-                    if headings_md[i] in line:
+                for i in range(0, len(head_mark)):
+                    if head_mark[i] in line:
                         new_line = line.replace(
-                            headings_md[i], headings_html[i])
-                        new_line += headings_html[i].replace("<", "</") + "\n"
+                            head_mark[i], head_html[i])
+                        new_line += head_html[i].replace("<", "</") + "\n"
                         f_out.write(new_line)
                         break
 
